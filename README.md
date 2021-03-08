b66a84797a6a1693e411b53aded48c7748d47a95
zhouguannan <kevinzhouguannan@outlook.com>
2021-03-08 20:58:45 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# MAKE THE KERNEL
+## A python script that can make and install patched kernel by KevinZhou
+
+## USE
+```bash
+git clone https://github.com/zhouguannan/make-the-kernel
+cd make-the-kernel
+python3 make-the-kernel.py
+```
+### please make shure your terminal has more than 80x80 char width
+
+# 初中生制作不易，点个star吧。
+# email：kevinzhouguannan@outlook.com
--- make-the-kernel.py
+++ make-the-kernel.py
@@ -2,7 +2,7 @@ import os
 print(
     "welcome,this program will install linux5.11.4+xanmod+uksm+cacule to your computer by it self(it's danger)"
 )
-print()
+print("Please make shure your terminal has 80x80 char")
 sysname = input("what\'s your system?(\"debian\"or\"arch\"):  ")
 print("installing build tools\n")
 if sysname == "debian":
@@ -12,13 +12,13 @@ elif sysname == "arch":
 cpucore = input("how many cpu core are in your cpu(enter a number): ")
 os.system("mkdir make-the-kernel")
 os.chdir("make-the-kernel")
-os.system("git clone -b 5.11 https://hub.fastgit.org/xanmod/linux")
 os.chdir("linux")
 os.system(
     "aria2c --split=100 https://raw.github.com/hamadmarri/cacule-cpu-scheduler/master/patches/CacULE/v5.11/cacule-5.11.patch"
 )
 os.system(
-    "aria2c --split=100 https://raw.github.com/dolohow/uksm/master/v5.x/uksm-5.11.patch")
+    "aria2c --split=100 https://raw.github.com/dolohow/uksm/master/v5.x/uksm-5.11.patch"
+)
 os.system("patch -p1 < uksm-5.11.patch")
 os.system("patch -p1 < cacule-5.11.patch")
 os.system("make menuconfig")
