fb574e93932c3199da068a6505ac96b95a072349
zhouguannan <kevinzhouguannan@outlook.com>
2021-03-08 20:33:04 +0800

    fix some bugs

--- make-the-kernel.py
+++ make-the-kernel.py
@@ -6,19 +6,19 @@ print()
 sysname = input("what\'s your system?(\"debian\"or\"arch\"):  ")
 print("installing build tools\n")
 if sysname == "debian":
-    os.system("sudo apt install git wget build-essential make gcc")
+    os.system("sudo apt install git wget build-essential make gcc aria2")
 elif sysname == "arch":
-    os.system("sudo pacman -S git wget make base-devel gcc")
+    os.system("sudo pacman -S git wget make base-devel gcc aria2")
 cpucore = input("how many cpu core are in your cpu(enter a number): ")
 os.system("mkdir make-the-kernel")
 os.chdir("make-the-kernel")
 os.system("git clone -b 5.11 https://hub.fastgit.org/xanmod/linux")
 os.chdir("linux")
 os.system(
-    "wget https://raw.github.com/hamadmarri/cacule-cpu-scheduler/master/patches/CacULE/v5.11/cacule-5.11.patch"
+    "aria2c --split=100 https://raw.github.com/hamadmarri/cacule-cpu-scheduler/master/patches/CacULE/v5.11/cacule-5.11.patch"
 )
 os.system(
-    "wget https://raw.github.com/dolohow/uksm/master/v5.x/uksm-5.11.patch")
+    "aria2c --split=100 https://raw.github.com/dolohow/uksm/master/v5.x/uksm-5.11.patch")
 os.system("patch -p1 < uksm-5.11.patch")
 os.system("patch -p1 < cacule-5.11.patch")
 os.system("make menuconfig")
